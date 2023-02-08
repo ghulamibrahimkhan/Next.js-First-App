17630bbbeb1eb0a9aeabba5f2f80c2c1991b956d
ghulamibrahimkhan <gulamibrahimkhan.com>
2023-02-09 03:13:47 +0500

    Editing the page

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Loraem Ipsim Dolla Dihn Pings Fassd Blsa Balasd slwfa vlpag</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' Google '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
